a631166496205c8b2067a5c878e9ed69939d365e
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2023-04-07 21:14:13 -0400

    remove gallery

--- gallery/js/script.js
+++
@@ -1,9 +0,0 @@
-/*=== lazyLoading ===*/
-window.addEventListener('load', function(){
-	var bLazy = new Blazy;
-}, false);
-
-/*=== Message close ===*/
-document.getElementById('closeMessage').addEventListener('click', function(){
-	document.getElementById('Message').style.display = 'none';
-});
